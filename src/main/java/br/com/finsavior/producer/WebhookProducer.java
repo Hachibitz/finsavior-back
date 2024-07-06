b764d883bee821c01c3e168ef148de93b3a31778
Hachibitz <felipealmeida.ns@outlook.com>
2024-07-05 21:11:51 -0300

    TASK-114 - feat: migração do consumer webhook para o events-processor

--- src/main/java/br/com/finsavior/producer/WebhookProducer.java
+++ src/main/java/br/com/finsavior/producer/WebhookProducer.java
@@ -4,6 +4,8 @@ package br.com.finsavior.producer;
 import java.util.UUID;
 
 import br.com.finsavior.model.dto.WebhookRequestDTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -15,14 +17,18 @@ import org.springframework.stereotype.Component;
 public class WebhookProducer {
 
     private final String topicName;
-    private final KafkaTemplate<String, WebhookRequestDTO> kafkaTemplate;
+    private final KafkaTemplate<String, String> kafkaTemplate;
 
-    public WebhookProducer(@Value("${webhook.request.topic.name}") String topicName, KafkaTemplate<String, WebhookRequestDTO> kafkaTemplate) {
+    private final ObjectMapper objectMapper;
+
+    public WebhookProducer(@Value("${webhook.request.topic.name}") String topicName, KafkaTemplate<String, String> kafkaTemplate,
+                           ObjectMapper objectMapper) {
         this.topicName = topicName;
         this.kafkaTemplate = kafkaTemplate;
+        this.objectMapper = objectMapper;
     }
 
-    private static void accept(SendResult<String, WebhookRequestDTO> result, Throwable ex) {
+    private static void accept(SendResult<String, String> result, Throwable ex) {
         if (ex != null) {
             log.error("Falha no envio: {}", ex.getMessage());
         } else {
@@ -30,8 +36,10 @@ public class WebhookProducer {
         }
     }
 
-    public void sendMessage(WebhookRequestDTO message){
+    public void sendMessage(WebhookRequestDTO message) throws JsonProcessingException {
         String id = UUID.randomUUID().toString();
-        kafkaTemplate.send(topicName, id, message).whenComplete(WebhookProducer::accept);
+        String messageJson = objectMapper.writeValueAsString(message);
+        log.info("Publishing message: {}", message);
+        kafkaTemplate.send(topicName, id, messageJson).whenComplete(WebhookProducer::accept);
     }
 }
--- src/main/java/br/com/finsavior/service/impl/WebhookServiceImpl.java
+++ src/main/java/br/com/finsavior/service/impl/WebhookServiceImpl.java
@@ -23,32 +23,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class WebhookServiceImpl implements WebhookService {
 
-    private final ExternalUserRepository externalUserRepository;
-    private final UserService userService;
-    private final ExternalUserMapper externalUserMapper;
     private final WebhookProducer webhookProducer;
-
-    @Override
-    public void processWebhook(WebhookRequestDTO webhookRequestDTO) {
-        ExternalUserDTO externalUserdto;
-        try {
-            externalUserdto = externalUserMapper.toExternalUserDTO(
-                    externalUserRepository.findBySubscriptionId(webhookRequestDTO.getResource().getId())
-            );
-
-            switch(webhookRequestDTO.getEvent_type()){
-                case BILLING_SUBSCRIPTION_ACTIVATED -> activatedEvent(externalUserdto, webhookRequestDTO);
-                case BILLING_SUBSCRIPTION_EXPIRED -> expiredEvent(externalUserdto);
-                case BILLING_SUBSCRIPTION_CANCELLED -> cancelledEvent(externalUserdto);
-                case BILLING_SUBSCRIPTION_PAYMENT_FAILED -> paymentFailedEvent(externalUserdto);
-                case BILLING_SUBSCRIPTION_SUSPENDED -> suspendedEvent(externalUserdto);
-                default -> throw new EventNotFound("Evento não mapeado!");
-
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     @Override
     public ResponseEntity<?> sendMessage(WebhookRequestDTO webhookRequestDTO) {
@@ -58,64 +33,5 @@ public class WebhookServiceImpl implements WebhookService {
             log.error("Error while sending message for webhook event, error = {}", e.getMessage());
         }
         return  ResponseEntity.ok().build();
-    }
-
-    private void suspendedEvent(ExternalUserDTO externalUser) {
-        try {
-            downgradeUserPlan(externalUser);
-        } catch (Exception e) {
-            throw new BusinessException("Error ao atualizar plano do usuário: " + externalUser.getUserId());
-        }
-    }
-
-    private void paymentFailedEvent(ExternalUserDTO externalUser) {
-        try {
-            downgradeUserPlan(externalUser);
-        } catch (Exception e) {
-            throw new BusinessException("Error ao atualizar plano do usuário: " + externalUser.getUserId());
-        }
-    }
-
-    private void cancelledEvent(ExternalUserDTO externalUser) {
-        try {
-            downgradeUserPlan(externalUser);
-        } catch (Exception e) {
-            throw new BusinessException("Error ao atualizar plano do usuário: " + externalUser.getUserId());
-        }
-    }
-
-    private void expiredEvent(ExternalUserDTO externalUser) {
-        try {
-            downgradeUserPlan(externalUser);
-        } catch (Exception e) {
-            throw new BusinessException("Error ao atualizar plano do usuário: " + externalUser.getUserId());
-        }
-    }
-
-    private void activatedEvent(ExternalUserDTO externalUser, WebhookRequestDTO webhookRequestDTO) {
-        try {
-            upgradeUserPlan(externalUser, webhookRequestDTO);
-        } catch (Exception e) {
-            throw new BusinessException("Error ao atualizar plano do usuário: " + externalUser.getUserId());
-        }
-    }
-
-    private void createdEvent(ExternalUserDTO externalUser, WebhookRequestDTO webhookRequestDTO) {
-    }
-
-    private void downgradeUserPlan(ExternalUserDTO externalUser) {
-        if((Objects.equals(externalUser.getPlanId(), PlanType.PLUS.getPlanTypeId()) ||
-                Objects.equals(externalUser.getPlanId(), PlanType.PREMIUM.getPlanTypeId()))){
-            externalUser.setPlanId(PlanType.FREE.getPlanTypeId());
-            userService.updateUserPlan(externalUser);
-        }
-    }
-
-    private void upgradeUserPlan(ExternalUserDTO externalUser, WebhookRequestDTO webhookRequestDTO) {
-        if(Objects.equals(externalUser.getPlanId(), PlanType.FREE.getPlanTypeId())){
-            externalUser.setPlanId(
-                    PlanType.fromValue(webhookRequestDTO.getResource().getPlanId()).getPlanTypeId());
-            userService.updateUserPlan(externalUser);
-        }
     }
 }
