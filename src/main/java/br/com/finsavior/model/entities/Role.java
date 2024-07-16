d4f58965445fc05915dc45f5febd2251dbdd5d36
Hachibitz <felipealmeida.ns@outlook.com>
2024-07-16 12:01:08 -0300

    hotfix: ajuste ne entity roles

--- src/main/java/br/com/finsavior/model/entities/Role.java
+++ src/main/java/br/com/finsavior/model/entities/Role.java
@@ -1,22 +1,16 @@
 package br.com.finsavior.model.entities;
 
-import java.util.Objects;
-
 import br.com.finsavior.model.enums.UserRoleEnum;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "roles")
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
